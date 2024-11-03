f313f11b55f12c4b89e54faa74ee6a4bd62b19d9
UyenPun <tranuyen2018@gmail.com>
2024-11-03 16:09:52 +0700

    03 Xây dựng danh sách việc cần làm.

--- vite-project/README.md
+++ vite-project/README.md
@@ -4,3 +4,7 @@ npm create vite@latest
 cd vite-project
 npm install
 npm run dev
+
+# Component là gì
+
+- là 1 phần UI có thể nhỏ như các button, hoặc là lớn như 1 Page
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,33 @@
 import "./App.css";
 
 function App() {
-  return <p>Hello</p>;
+  return (
+    <div>
+      <input
+        type='text'
+        name='add-new-task'
+        placeholder='Add new task'
+      />
+
+      <div>
+        <div>
+          <p>Đi học thêm</p>
+        </div>
+        <div>
+          <p>Đi học bài</p>
+        </div>
+        <div>
+          <p>Đi học võ</p>
+        </div>
+        <div>
+          <p>Đi chợ</p>
+        </div>
+        <div>
+          <p>Đi ngủ</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
